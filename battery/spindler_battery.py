daf8de5651ecfd5d8dd5cdc43950cc7b1c752604
Taha Ozair Osman <taha2013.to@gmail.com>
2022-11-26 11:58:14 +0000

    Added date calculation

--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -1,4 +1,5 @@
 from battery.battery import Battery
+from utils import add_years_to_date
 
 class SpindlerBattery(Battery):
     def __init__(self, last_service_date, current_date):
@@ -6,4 +7,5 @@ class SpindlerBattery(Battery):
         self.current_date = current_date
 
     def needs_service(self):
-        return self.current_date - self.last_service_date > 2
+        date_which_battery_should_be_serviced_by = add_years_to_date(self.last_service_date, 2)
+        return self.current_date > date_which_battery_should_be_serviced_by
