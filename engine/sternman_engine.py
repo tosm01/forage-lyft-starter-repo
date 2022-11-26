ae6b978d09a1318c365e715dfda1f32f4df0bf0c
Taha Ozair Osman <taha2013.to@gmail.com>
2022-11-26 12:35:50 +0000

    Fixed error in sternman engine

--- engine/sternman_engine.py
+++ engine/sternman_engine.py
@@ -6,7 +6,4 @@ class SternmanEngine(Engine):
         self.warning_light_on = warning_light_on
 
     def needs_service(self):
-        if self.warning_light_is_on:
-            return True
-        else:
-            return False
+        return self.warning_light_on
